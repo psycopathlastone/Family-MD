18652772a109545929657c835be8351b03e1d4c9
psycopathlastone <110118297+psycopathlastone@users.noreply.github.com>
2022-07-28 16:10:06 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -20,7 +20,7 @@ module.exports = {
         if (!m) return
         
         global.namabot = await this.user.name
-        global.wm = namabot + ' ву ƒσкυѕ ι∂'
+        global.wm = namabot + ' ву αη∂яιαη ℓαѕтσηє'
         //console.log(JSON.stringify(m, null, 4))
         try {
             m = simple.smsg(this, m) || m
